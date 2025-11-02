4b6cdfd869048733f744f7bb34ca4ddb6bb06bc2
anyikwadora-png <anyikwadora@gmail.com>
2025-11-02 18:54:13 +0100

    added build.sh

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# build.sh
+set -e  # exit on error
+
+# Install a specific Python version via pyenv (Render should have pyenv available)
+echo "Installing Python 3.10.14..."
+pyenv install -s 3.10.14
+pyenv global 3.10.14
+
+# Upgrade pip and setuptools first
+pip install --upgrade pip setuptools wheel
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Any other build steps you need, like migrations
+# python manage.py migrate
