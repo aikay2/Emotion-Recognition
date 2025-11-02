915b8f4413bab8010b562f20883931840c7576e3
anyikwadora-png <anyikwadora@gmail.com>
2025-11-02 18:59:20 +0100

    added build.sh edited

--- build.sh
+++ build.sh
@@ -1,17 +1,24 @@
-#!/usr/bin/env bash
-# build.sh
-set -e  # exit on error
+# #!/usr/bin/env bash
+# # build.sh
+# set -e  # exit on error
 
-# Install a specific Python version via pyenv (Render should have pyenv available)
-echo "Installing Python 3.10.14..."
-pyenv install -s 3.10.14
-pyenv global 3.10.14
+# # Install a specific Python version via pyenv (Render should have pyenv available)
+# echo "Installing Python 3.10.14..."
+# pyenv install -s 3.10.14
+# pyenv global 3.10.14
 
-# Upgrade pip and setuptools first
-pip install --upgrade pip setuptools wheel
+# # Upgrade pip and setuptools first
+# pip install --upgrade pip setuptools wheel
 
-# Install dependencies
+# # Install dependencies
+# pip install -r requirements.txt
+
+# # Any other build steps you need, like migrations
+# # python manage.py migrate
+
+set -o errexit
+
 pip install -r requirements.txt
 
-# Any other build steps you need, like migrations
-# python manage.py migrate
+python manage.py collectstatic --no-input
+python manage.py migrate
